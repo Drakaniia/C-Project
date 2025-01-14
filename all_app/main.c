d650cd31f74d51b4e7bc65d1700d8908c98c0ba8
jp-web-space <2401101011@student.buksu.edu.ph>
2025-01-13 20:32:56 -0800

    Add files via upload

---
+++ all_app/main.c
@@ -0,0 +1,50 @@
+#include "temperature_bank.h"
+#include <stdio.h>
+#include <string.h>
+
+// MAIN METHOD FOR EXECUTION
+
+int main() {
+  int appChoice;
+  char againChoice[10];
+
+  do {
+    // Menu display
+    mod5_menuPrompts();
+
+    // Application choice with validation
+    appChoice = valid1_appChoice();
+
+    switch (appChoice) {
+      case 3:
+        mod3_tempConversion();
+        break;
+      case 4:
+        mod4_bankSystem();
+        break;
+      default:
+        printf("Invalid Choice! Please try again.\n");
+        break;
+    }
+
+    // Another app choice with validation
+    valid2_againChoice(againChoice, sizeof(againChoice));
+
+  } while (strcasecmp(againChoice, "Yes") != 0);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ all_app/temperature_bank.h
@@ -0,0 +1,18 @@
+#ifndef TEMPERATURE_BANK_H
+#define TEMPERATURE_BANK_H
+
+// TEMPERATURE CONVERSION MODULE
+void mod3_tempConversion();
+
+// BANK MANAGEMENT SYSTEM MODULE
+void mod4_bankSystem();
+
+// MENU DISPLAY AND VALIDATION MODULE
+void mod5_menuPrompts();
+
+// Input validation prototypes
+int valid1_appChoice();
+char* valid2_againChoice(char* againChoice, int valid_size);
+
+
+#endif // TEMPERATURE_BANK_H
